77ce3f0ec7443d1cbce16e3bd60391e040df37cd
Brian Hume <brian@Brians-MacBook-Pro.local>
2018-12-05 18:53:17 -0300

    Updates

---
+++ precache-manifest.f4b58b7a3b8a72b54018958749e3c800.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "095a8315a750474731cc",
+    "url": "/poker-app/static/css/main.07d14baf.chunk.css"
+  },
+  {
+    "revision": "095a8315a750474731cc",
+    "url": "/poker-app/static/js/main.095a8315.chunk.js"
+  },
+  {
+    "revision": "324b6cf3b56bf43378ce",
+    "url": "/poker-app/static/js/1.324b6cf3.chunk.js"
+  },
+  {
+    "revision": "27aeb35e92e59cc666a7",
+    "url": "/poker-app/static/js/runtime~main.27aeb35e.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/poker-app/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "d89f9f569161773163228feb31806474",
+    "url": "/poker-app/index.html"
+  }
+];
