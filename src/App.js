5ab3dec22a7547a41b25f1cfd0fea627135c11d5
Brian Hume <brian@Brians-MacBook-Pro.local>
2018-12-05 18:47:05 -0300

    Added poker app

--- src/App.js
+++ src/App.js
@@ -2,24 +2,93 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+import config from './support';
+
 class App extends Component {
+  constructor(props) {
+    super(props)
+
+    this.state = {
+      mainOption: null,
+      subOption: null,
+      config: config
+    }
+  }
+
+  selectMainOption(option) {
+    this.setState({
+      mainOption: option,
+      subOption: null,
+    })
+  }
+
+  selectSubOption(option) {
+    this.setState({
+      subOption: option
+    })
+  }
+
+  mainOptions() {
+    return this.state.config.map((list, index) => {
+      return (
+        <a
+          style={{ 
+            cursor: 'pointer', 
+            backgroundColor: index === this.state.mainOption -1 && '#E3B23C'
+          }}
+          onClick={() => { this.selectMainOption(list.value) }}>
+          {list.label}
+        </a>)
+    })
+  }
+
+  subOptions(mainOption) {
+    return this.state.config.filter(list => list.value === mainOption).map(sublist => {
+      return sublist.suboptions.map(optionsRow => {
+        return (
+          <ul style={{
+            display: 'flex',
+            justifyContent: 'space-around',
+            listStyle: 'none',
+            padding: 0,
+            margin: 0,
+            width: '100%'
+          }}
+          >
+            {optionsRow.map((option) => (
+              <li
+                style={{
+                  flex: '1',
+                  padding: '.2em',
+                  margin: '.1em',
+                  cursor: 'pointer',
+                  backgroundColor: option.value === this.state.subOption ? '#A39594' : '#EDEBD7',
+                  border: '1px solid black',
+                  borderRadius: '.2em',
+                  textAlign: 'center'
+                }}
+                onClick={() => { this.selectSubOption(option.value) }}>
+                {option.label}
+              </li>))}
+          </ul>
+        )
+      })
+    })
+  }
+
   render() {
+    const { mainOption, subOption } = this.state
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        <div className="mainOptions">
+          {this.mainOptions(config)}
+        </div>
+        <div className="subOptions">
+          {this.subOptions(mainOption)}
+        </div>
+        <div className="image">
+          {mainOption && subOption && <img src={`./assets/${mainOption}${subOption}.jpg`} />}
+        </div>
       </div>
     );
   }
---
+++ src/support.js
@@ -0,0 +1,249 @@
+export default [
+  {
+    label: 'BB 3H',
+    value: 1,
+    suboptions: [
+      [{
+        label: '25-16 SB LIMP',
+        value: 'AA'
+      },
+      {
+        label: '16-12 SB LIMP',
+        value: 'AB'
+      },
+      {
+        label: '12-10 SB LIMP',
+        value: 'AC'
+      },
+      {
+        label: '10-8 SB LIMP',
+        value: 'AD'
+      }],
+      [{
+        label: '25-16 SB MR',
+        value: 'BA'
+      },
+      {
+        label: '16-12 SB MR',
+        value: 'BB'
+      },
+      {
+        label: '12-10 SB MR',
+        value: 'BC'
+      },
+      {
+        label: '10-8 SB MR',
+        value: 'BD'
+      }],
+      [{
+        label: '25-16 BU LIMP',
+        value: 'CA'
+      },
+      {
+        label: '16-12 BU LIMP',
+        value: 'CB'
+      },
+      {
+        label: '12-10 BU LIMP',
+        value: 'CC'
+      },
+      {
+        label: '10-8 BU LIMP',
+        value: 'CD'
+      }],
+      [{
+        label: '25-16 BU MR',
+        value: 'DA'
+      },
+      {
+        label: '16-12 BU MR',
+        value: 'DB'
+      },
+      {
+        label: '12-10 BU MR',
+        value: 'DC'
+      },
+      {
+        label: '10-8 BU MR',
+        value: 'DD'
+      }]
+    ]
+  },{
+    label: 'SB 3H',
+    value: 2,
+    suboptions: [
+      [{
+        label: '25-16 BU FOLD',
+        value: 'AA'
+      },
+      {
+        label: '16-12 BU FOLD',
+        value: 'AB'
+      },
+      {
+        label: '12-10 BU FOLD',
+        value: 'AC'
+      },
+      {
+        label: '10-8 BU FOLD',
+        value: 'AD'
+      }],
+      [{
+        label: '25-16 BU LIMP',
+        value: 'BA'
+      },
+      {
+        label: '16-12 BU LIMP',
+        value: 'BB'
+      },
+      {
+        label: '12-10 BU LIMP',
+        value: 'BC'
+      },
+      {
+        label: '10-8 BU LIMP',
+        value: 'BD'
+      }],
+      [{
+        label: '25-16 BU MR',
+        value: 'CA'
+      },
+      {
+        label: '16-12 BU MR',
+        value: 'CB'
+      },
+      {
+        label: '12-10 BU MR',
+        value: 'CC'
+      },
+      {
+        label: '10-8 BU MR',
+        value: 'CD'
+      }],
+      [{
+        label: '25-16 BU OS',
+        value: 'DA'
+      },
+      {
+        label: '16-12 BU OS',
+        value: 'DB'
+      },
+      {
+        label: '12-10 BU OS',
+        value: 'DC'
+      },
+      {
+        label: '10-8 BU OS',
+        value: 'DD'
+      }]
+    ]
+  },
+  {
+    label: 'BU 3H',
+    value: 3,
+    suboptions: [
+      [{
+        label: '25-16 BU OPEN',
+        value: 'A'
+      },
+      {
+        label: '16-12 BU OPEN',
+        value: 'B'
+      },
+      {
+        label: '12-10 BU OPEN',
+        value: 'C'
+      },
+      {
+        label: '10-8 BU OPEN',
+        value: 'D'
+      }]
+    ]
+  },
+  {
+    label: 'BB HU',
+    value: 4,
+    suboptions: [
+      [{
+        label: '25-16 SB LIMP',
+        value: 'AA'
+      },
+      {
+        label: '16-12 SB LIMP',
+        value: 'AB'
+      },
+      {
+        label: '10-8 SB LIMP',
+        value: 'AC'
+      }],
+      [{
+        label: '25-16 SB MR',
+        value: 'BA'
+      },
+      {
+        label: '16-12 SB MR',
+        value: 'BB'
+      },
+      {
+        label: '12-10 SB MR',
+        value: 'BC'
+      },
+      {
+        label: '10-8 SB MR',
+        value: 'BD'
+      }]
+    ]
+  },
+  {
+    label: 'SB HU',
+    value: 5,
+    suboptions: [
+      [{
+        label: '25-16 SB OPEN',
+        value: 'A'
+      },
+      {
+        label: '16-12 SB OPEN',
+        value: 'B'
+      },
+      {
+        label: '12-10 SB OPEN',
+        value: 'C'
+      },
+      {
+        label: '10-8 SB OPEN',
+        value: 'D'
+      }]
+    ]
+  },
+  {
+    label: 'NASH',
+    value: 6,
+    suboptions: [
+      [{
+        label: 'HU CALL',
+        value: 'AA'
+      },
+      {
+        label: 'HU PUSH',
+        value: 'AB'
+      }],
+      [{
+        label: 'VS BU+SB',
+        value: 'BA'
+      },
+      {
+        label: 'BB vs BTN',
+        value: 'BB'
+      },
+      {
+        label: 'SB vs BTN',
+        value: 'BC'
+      },
+      {
+        label: 'BTN',
+        value: 'BD'
+      }]
+    ]
+  },
+];
